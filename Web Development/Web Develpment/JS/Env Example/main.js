5a6acef7ab8c4435359d5fed6c88558b20cabc95
Abnaby <31739953+Abnaby@users.noreply.github.com>
2024-08-10 21:41:18 +0300

    DOM

--- Web Development/Web Develpment/JS/Env Example/main.js
+++ Web Development/Web Develpment/JS/Env Example/main.js
@@ -1,4 +1,11 @@
-// Get the first <p> element in the document
-const paragraph = document.querySelector("p");
-const attributes = paragraph.attributes;
-console.log(attributes[0]);
+/*
+  DOM [Cloning]
+  - cloneNode(Deep)
+*/
+
+let myP = document.querySelector("p").cloneNode(true);
+let myDiv = document.querySelector("div");
+
+myP.id = `${myP.id}-clone`;
+
+myDiv.appendChild(myP);
